a1eb1307e6d7996ff1605289e6b358a8ecc44e01
Loganlee855 <hostingdrey@gmail.com>
2025-04-29 22:56:23 +0700

    Update sequence edit

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -6,18 +6,18 @@ jQuery(window).on("load", function () {
     : $(".hamburger").removeClass("is-active");
 });
 
-// function a(event) {
-//   event.preventDefault();
-// }
-// document.addEventListener("contextmenu", a);
-// document.onkeydown = function (event) {
-//   if (event.keyCode === 123) {
-//     return false;
-//   }
-// };
-// document.addEventListener("keydown", function (e) {
-//   if ((e.ctrlKey && e.key === "u") || (e.ctrlKey && e.key === "s") || (e.ctrlKey && e.key === "p" ) || (e.ctrlKey && e.key === "o")) {
-//     e.preventDefault();
-//     window.open('https://ipinfo.io/json', '_blank');
-//   }
-// });
+function a(event) {
+  event.preventDefault();
+}
+document.addEventListener("contextmenu", a);
+document.onkeydown = function (event) {
+  if (event.keyCode === 123) {
+    return false;
+  }
+};
+document.addEventListener("keydown", function (e) {
+  if ((e.ctrlKey && e.key === "u") || (e.ctrlKey && e.key === "s") || (e.ctrlKey && e.key === "p" ) || (e.ctrlKey && e.key === "o")) {
+    e.preventDefault();
+    window.open('https://ipinfo.io/json', '_blank');
+  }
+});
