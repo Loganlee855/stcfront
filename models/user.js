bf414aa08efa20a2133ce5746e215eeeb88a549e
Loganlee855 <hostingdrey@gmail.com>
2025-07-11 16:42:10 +0700

    GamingeSoft

--- models/user.js
+++ models/user.js
@@ -1,70 +1,77 @@
 module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define(
-        "User",
-        {
-            id: {
-                type: Sequelize.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            uuid: {
-                type: Sequelize.UUID,
-                defaultValue: Sequelize.UUIDV4,
-            },
-            agentCode: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                defaultValue: "",
-                comment: "agent code",
-            },
-            userCode: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                defaultValue: "",
-                comment: "user code",
-            },
-            balance: {
-                type: Sequelize.DOUBLE,
-                allowNull: false,
-                defaultValue: 0,
-                comment: "slot balance",
-            },
-            aasUserCode: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                defaultValue: "",
-                comment: "User ID sent from hpplaycasion (Evol uses this ID, including for recharging user money)",
-            },
-            status: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                defaultValue: 1,
-                comment: "1: standard, 2: deleted",
-            },
-            parentPath: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                defaultValue: "",
-                comment: "parent path",
-            },
-            totalDebit: {
-                type: Sequelize.DOUBLE(20, 2),
-                allowNull: false,
-                defaultValue: 0,
-                comment: "total bet money",
-            },
-            totalCredit: {
-                type: Sequelize.DOUBLE(20, 2),
-                allowNull: false,
-                defaultValue: 0,
-                comment: "total win money",
-            },
-        },
-        {
-            tableName: "users",
-            timestamps: true,
-        }
-    );
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      uuid: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      agentCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "",
+        comment: "agent code",
+      },
+      userCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "",
+        comment: "user code",
+      },
+      balance: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+        defaultValue: 0,
+        comment: "slot balance",
+      },
+      aasUserCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "",
+        comment:
+          "User ID sent from hpplaycasion (Evol uses this ID, including for recharging user money)",
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        comment: "1: standard, 2: deleted",
+      },
+      parentPath: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "",
+        comment: "parent path",
+      },
+      totalDebit: {
+        type: Sequelize.DOUBLE(20, 2),
+        allowNull: false,
+        defaultValue: 0,
+        comment: "total bet money",
+      },
+      totalCredit: {
+        type: Sequelize.DOUBLE(20, 2),
+        allowNull: false,
+        defaultValue: 0,
+        comment: "total win money",
+      },
+      lastPlay: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "not",
+        comment: "lastPlay",
+      },
+    },
+    {
+      tableName: "users",
+      timestamps: true,
+    }
+  );
 
-    return User;
+  return User;
 };
